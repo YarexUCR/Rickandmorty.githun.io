cd0f0650085b2ed137c7dc6cd377c0cd612b5771
Yarex17 <159191461+YarexUCR@users.noreply.github.com>
2024-05-22 22:06:41 -0600

    ddd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import imageRickMorty from "./img/rick-morty.png";
+import fondo from './img/fondo.jpg'; // Ajusta la ruta según la ubicación de tu imagen
 
 import "./App.css";
 import { useState } from "react";
 import Characters from "./Components/Characters";
 
 function App() {
+  const backgroundStyle = {
+    backgroundImage: `url(${fondo})`,
+    backgroundSize: 'cover', // Esto asegura que la imagen cubra toda el área del fondo
+    backgroundPosition: 'center', // Esto centra la imagen
+    height: '100vh', // Esto hace que el fondo ocupe toda la altura de la ventana
+    width: '100%', // Esto hace que el fondo ocupe todo el ancho del contenedor
+    margin: '0', // Elimina cualquier margen por defecto
+    padding: '0', // Elimina cualquier padding por defecto
+  };
   const [characters, setCharacters] = useState(null);
 
   const reqApi = async () => {
@@ -15,7 +25,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div style={backgroundStyle} className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty </h1>
         {characters ? (
